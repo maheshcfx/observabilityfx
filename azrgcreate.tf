1855a6d7b2ffdfb86ab25176201b7287d9d78de0
maheshcfx <131735852+maheshcfx@users.noreply.github.com>
2025-04-22 15:54:15 +0530

    Create azrgcreate.tf

---
+++ azrgcreate.tf
@@ -0,0 +1,47 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  
+  resource_provider_registrations = "none"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Define variables
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Location for the Resource Group"
+  type        = string
+}
